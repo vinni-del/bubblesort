dceb2f9c75f6ffeb4db44f812b812688d99953b0
vinni-del <59853693+vinni-del@users.noreply.github.com>
2021-11-27 04:03:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+             System.Console.WriteLine($"Array before sort");
+            int[] numbers = {21,55,44,11,66};
+                //System.Console.WriteLine("----------------");
+                System.Console.WriteLine(string.Join(',' , numbers));
+            for (int i =0; i < numbers.Length -1; i++){
+                for (int j =0; j <  numbers.Length -1-i;j++){
+                    if (numbers[j] > numbers[j+1]){
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j+1];
+                        numbers[j+1] = temp;
+                    }
+                }
+            }
+             System.Console.WriteLine("Sorted array:");
+                    System.Console.WriteLine(string.Join(',',numbers));
+        }
+    }
+}
