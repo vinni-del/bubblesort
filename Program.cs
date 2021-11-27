cb63edd19ff423e8f5b3194bb2751ab767919272
vinni-del <59853693+vinni-del@users.noreply.github.com>
2021-11-27 10:54:38 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 
-namespace Bubblesort
+namespace Armstrong
 {
     class Program
     {
         static void Main(string[] args)
-        {    
-             System.Console.WriteLine($"Array before sort");
-            int[] numbers = {21,55,44,11,66};
-                //System.Console.WriteLine("----------------");
-                System.Console.WriteLine(string.Join(',' , numbers));
-            for (int i =0; i < numbers.Length -1; i++){
-                for (int j =0; j <  numbers.Length -1-i;j++){
-                    if (numbers[j] > numbers[j+1]){
-                        int temp = numbers[j];
-                        numbers[j] = numbers[j+1];
-                        numbers[j+1] = temp;
-                    }
-                }
-            }
-             System.Console.WriteLine("Sorted array:");
-                    System.Console.WriteLine(string.Join(',',numbers));
+        {
+        System.Console.Write("Enter your input: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int num = number;
+        int AfterCubeValue = 0;
+        while(number > 0){
+            int remainder = number % 10;
+            number /= 10;
+            AfterCubeValue = AfterCubeValue + remainder*remainder*remainder;
+
+        }
+         if(num == AfterCubeValue) {
+        System.Console.WriteLine($"{num} is an armstrong number. ");
+         }
+         else{
+             System.Console.WriteLine($"{num} is not an armstrong number.");
+         }
         }
     }
 }
